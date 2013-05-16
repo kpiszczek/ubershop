13f26e797b24f448ec3ea6ca5f1a941e557d69a6
kpiszczek <jppiszczek@gmail.com>
2013-05-16 19:52:09 +0200

    urle dla EShop

--- ubershop/urls.py
+++ ubershop/urls.py
@@ -12,7 +12,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'ubershop.views.home', name='home'),
     # url(r'^ubershop/', include('ubershop.foo.urls')),
     url(r'^home/$','core.views.home_page'),
+    url(r'^eshop/(?P<id>\d+)/$', EShopView.show_item),
     url(r'^eshop/(?P<id>\d+)/get_pdf', EShopView.get_item_pdf),
+    url(r'^eshop/kategorie/(?P<category>.+)/$', EShopView.items_list),
+    url(r'^eshop/szukaj/(?P<term>.+)/$', EShopView.search_item),
+    url(r'^eshop/nowe/$', EShopView.newest_items),
+    url(r'^eshop/popoularne/$', EShopView.popular_items),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
