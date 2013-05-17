ede50481675e57b626fc3b7a85be5109304c9b33
Kmaryanski <krzysztof.maryanski3@gmail.com>
2013-05-17 17:14:04 +0200

    bid_history

--- auction/views.py
+++ auction/views.py
@@ -6,8 +6,12 @@ class AuctionView(BaseView):
     meta = AuctionItem
     
     @staticmethod
-    def bid_history(request):
-        raise NotImplemented
+    def bid_history(request, auction_id):
+        auction=AuctionItem.objects.all()
+        auction=AuctionItem.objects.filter(pk=auction_id)
+        bid_history=auction.bids
+        return render_to_response("bid_history.html",{'bid_history': bid_history, 'auction':auction},context_instance=RequestContext(request))
+        #raise NotImplemented
     
     @staticmethod
     def aution_panel(request):
