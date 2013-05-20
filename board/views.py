8bee03105bfa2f587e22d1681b28dfdbb0f63383
Kmaryanski <krzysztof.maryanski3@gmail.com>
2013-05-20 16:30:49 +0200

    csfr zwalczone, teraz przekierowania...

--- board/views.py
+++ board/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.core.context_processors import csrf
-from django.views.decorators.csrf import csrf_protect
+
 #from django.views.decorators.csrf import csrf_exempt
 
 from board.forms import TopicForm, MessageForm, BoardForm
@@ -36,17 +35,17 @@ class BoardView():
         return render_to_response("board_list.html",{'boards': forums},context_instance=RequestContext(request))
         #raise NotImplemented
     
-    #@csrf_exempt
+
     @classmethod
     def create_topic(cls,request, board_id):
         if request.method=='POST':
             topic_form=TopicForm(request.POST)
             new_topic=topic_form.save()
             redirect_url = reverse('topic', kwargs={'id': new_topic.pk})
-            return HttpResponseRedirect(redirect_url)
+            return HttpResponseRedirect(redirect_url, context_instance=RequestContext(request))
         else:
             topic_form=TopicForm()
-        return render_to_response('new_topic.html', {'form': topic_form})
+        return render_to_response('new_topic.html', {'form': topic_form}, context_instance=RequestContext(request))
         #raise NotImplemented
     
     
@@ -56,23 +55,23 @@ class BoardView():
             board_form=BoardForm(request.POST)
             new_board=board_form.save()
             redirect_url = reverse('board', kwargs={'id': new_board.pk})
-            return HttpResponseRedirect(redirect_url)
+            return HttpResponseRedirect(redirect_url, context_instance=RequestContext(request))
         else:
             board_form=BoardForm()
-        return render_to_response('new_board.html', {'board': board_form})
+        return render_to_response('new_board.html', {'board': board_form}, context_instance=RequestContext(request))
         #raise NotImplemented  
   
-    #@csrf_exempt
+
     @classmethod
     def submit_message(cls,request):
         if request.method=='POST':
             message_form=MessageForm(request.POST)
             new_message=message_form.save()
             redirect_url = reverse('topic', kwargs={'id': new_message.topic})
-            return HttpResponseRedirect(redirect_url)
+            return HttpResponseRedirect(redirect_url, context_instance=RequestContext(request))
         else:
             topic_form=TopicForm()
-        return render_to_response('new_message.html', {'form': message_form})
+        return render_to_response('new_message.html', {'form': message_form}, context_instance=RequestContext(request))
         #raise NotImplemented
     
     @classmethod
