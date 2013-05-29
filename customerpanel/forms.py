b926e34862cd2f7512068fba492db1bbc4b1a6c4
kpiszczek <jppiszczek@gmail.com>
2013-05-29 22:50:21 +0200

    formularz rejestracji

--- customerpanel/forms.py
+++ customerpanel/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from core.models import ShopUser
 
 class RegisterForm(forms.Form):
-    class Meta:
-        model = ShopUser
-        fields = ('username','password', 'first_name','last_name', 'email', 'organisation', 'address', 'tax_id')
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    address = forms.CharField(widget=forms.Textarea)
+    organisation = forms.CharField(required=False, widget=forms.Textarea)
+    tax_id = forms.CharField(required=False)
+    
+    
