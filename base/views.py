17f2f4b63a39c8cc22d3a7f29f89d9d27276bffb
kpiszczek <jppiszczek@gmail.com>
2013-06-02 10:30:02 +0200

    komentarze na stronie produktów

--- base/views.py
+++ base/views.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+from datetime import datetime
+
 from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.db.models import Q
 from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 from base.models import BaseItem
-from core.models import Category
+from core.models import Category, ShopUser
 from base.models import BaseItem
 from eshop.models import EShopItem
 from auction.models import AuctionItem
 from groupbuy.models import GroupOffer
 from base.forms import SearchForm
+from board.models import Topic, Message, Board
+from board.forms import MessageForm
 from pdfgenerator.generator import PdfGenerator
 
 def inject_search_form(view):
@@ -79,15 +85,55 @@ class BaseView():
         # DZIALA
         # cls.model w każdej klasie podklasie CośtamView jest podmieniany na odpowiednią klasę modelu.
         # cls.model odpowiada EShopItem, AuctionItem, GroupOffer w zależnosci od klasy, z której zostanie wywołane.
+        
         item = cls.model.objects.get(pk=id)
-        data = {"item": item, "categories": cls.get_categories()}
+        try:
+            topic = Topic.objects.filter(board__name=str(cls.model.__name__), title=str(id))[0]
+            comments = topic.messages.all()
+        except ValueError:
+            comments = []
+        except IndexError:
+            comments = []
+        data = {"item": item, "categories": cls.get_categories(), 
+                "comments": comments, "comment_form": MessageForm()}
         if injected is not None:
             data.update(injected)
+        
         # raise Http404(item.base.name)
         # podmieniamy początek nazwy szablony na nazwę klasy modelu (pisanej małymi literami)
         return render_to_response("%s_detail.html" % cls.model.__name__.lower(),
                                   data,
                                   context_instance=RequestContext(request))
+    
+    @classmethod
+    @method_decorator(login_required(login_url='/accounts/login/'))  
+    def comment(cls, request, id):
+        if request.method == "POST":
+            form = MessageForm(request.POST)
+            if form.is_valid():
+                content = form.cleaned_data["content"]
+                
+                try:
+                    topic = Topic.objects.get(board__name=str(cls.model.__name__), title=str(id))
+                except Exception:
+                    board = Board.objects.get(name=str(cls.model.__name__))
+                    title = str(id)
+                    date = datetime.now()
+                    created_by = ShopUser.objects.get(user__pk=request.user.pk)
+                    topic = Topic(title=title, board=board, created_by=created_by, date=date, is_active=True)
+                    topic.save()
+                    
+                submission_date = datetime.now()
+                submitted_by = ShopUser.objects.get(user__pk=request.user.pk)
+                
+                new_message = Message(topic=topic, submitted_by=submitted_by, submission_date=submission_date, content=content)
+                #new_message=Message(topic=topic, submitted_by=submitted_by, submission_date=submission_date, content=content)
+                new_message.save()
+                topic.messages.add(new_message)
+                topic.save()
+
+                return cls.show_item(request, id)
+        return cls.show_item(request, id)
     
     @classmethod
     def get_item_pdf(cls, request, id):
